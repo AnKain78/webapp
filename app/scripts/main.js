e277ed6bffc4aa2caf07752416344adfe950250e
AnKain78 <33071818+AnKain78@users.noreply.github.com>
2017-11-17 23:35:58 +0100

    added functional programming samples

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//var squareNumbers = numbers.map(function(num) {return num * num});
+let squareNumbers = numbers.map((num) => num * num);
+let productNumbersSquareNumbers = numbers.map((num, index) => {
+    return num * squareNumbers[index];
+})
+
+console.log(numbers);
+console.log(squareNumbers);
+console.log(productNumbersSquareNumbers);
