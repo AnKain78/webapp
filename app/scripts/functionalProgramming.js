3966cbbed117538da13b4c79792047aa4cba478e
AnKain78 <33071818+AnKain78@users.noreply.github.com>
2017-11-18 00:27:03 +0100

    put functional programming samples into an extra function

---
+++ app/scripts/functionalProgramming.js
@@ -0,0 +1,21 @@
+function createNumbers(count) {
+    let arr = []
+    for (var i = 1; i <= count; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
+  
+  let numbers = createNumbers(10);
+  
+  
+  //var squareNumbers = numbers.map(function(num) {return num * num});
+  let squareNumbers = numbers.map((num) => num * num);
+  let productNumbersSquareNumbers = numbers.map((num, index) => {
+    return num * squareNumbers[index];
+  })
+  
+  console.log(numbers);
+  console.log(squareNumbers);
+  console.log(productNumbersSquareNumbers);
+  
